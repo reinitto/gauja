237dbdc99a7ae5b89e6059a2f21b79e1cdebaa9d
Labais <reinitto@gmail.com>
2019-03-21 19:25:45 +0100

    add background image, section style

--- public/app.js
+++ public/app.js
@@ -1,6 +1,12 @@
 window.addEventListener("DOMContentLoaded", event => {
   console.log("DOM fully loaded and parsed");
   const projectList = document.querySelector(".projectList");
+  const send = document.querySelector("#send");
+  const email = document.querySelector("#email");
+  const message = document.querySelector("#message");
+  const sentMessage = document.querySelector("#cm");
+  const thumbs = document.querySelector("#thumbs");
+  const formMessage = document.querySelector("#formMessage");
   let screenshotLinks = [
     "img/screenshot1.png",
     "img/screenshot2.png",
@@ -11,5 +17,47 @@ window.addEventListener("DOMContentLoaded", event => {
       "beforeend",
       ` <div class="projectItem"> <img src=${link} alt="project screenshot image" /> <hr /> <div class="projectDescription"><p>some project description</p></div> </div>`
     );
+  });
+
+  send.addEventListener("click", () => {
+    const Url = "/sendEmail";
+    const Data = {
+      email: email.value,
+      message: message.value
+    };
+    const params = {
+      method: "POST",
+      body: JSON.stringify(Data),
+      headers: { "content-type": "application/json; charset=UTF-8" }
+    };
+
+    fetch(Url, params)
+      .then(res => res.json())
+      .then(res => {
+        if (res.ok) {
+          sentMessage.classList.remove("contactSent");
+          sentMessage.classList.remove("contactFail");
+          sentMessage.classList.add("contactSuccess");
+          thumbs.classList.remove("fa-thumbs-down");
+          thumbs.classList.add("fa-thumbs-up");
+          formMessage.textContent = "Message successfully sent!";
+          email.value = "";
+          message.value = "";
+        } else {
+          sentMessage.classList.remove("contactSent");
+          sentMessage.classList.remove("contactSuccess");
+          sentMessage.classList.add("contactFail");
+          thumbs.classList.remove("fa-thumbs-up");
+          thumbs.classList.add("fa-thumbs-down");
+          formMessage.textContent = "Failed to send message! Please try again.";
+        }
+      })
+      .catch(err => console.log("error occured, try again"));
+
+    setTimeout(function() {
+      sentMessage.classList.remove("contactFail");
+      sentMessage.classList.remove("contactSuccess");
+      sentMessage.classList.add("contactSent");
+    }, 2500);
   });
 });
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require("express");
-
+const emailValidator = require("email-validator");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Public folder
 app.use(express.static(__dirname + "/public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.sendFile("./index.html");
+});
+
+app.post("/sendEmail", (req, res) => {
+  if (emailValidator.validate(req.body.email)) {
+    res.send({
+      ok: true,
+      status: 200
+    });
+  } else
+    res.send({
+      ok: false,
+      status: 404
+    });
 });
 
 app.listen(PORT, () => console.log(`serving site on ${PORT}`));
